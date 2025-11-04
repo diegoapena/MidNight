9d7b8b366a49d259670c652273f5cdbb3d6cfc19
rolandoparedes-sketch <rolando.paredes@tecsup.edu.pe>
2025-11-03 23:26:26 -0500

    Acabado Linterna

---
+++ Assets/Scripts/LinternaController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LinternaController : MonoBehaviour
+{
+    public Light2D flashlight;
+    public float rotationSpeed = 8f;
+    private bool isOn = false;
+    private Vector2 lastMoveDir = Vector2.down;
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveDir = new Vector2(moveX, moveY);
+
+        if (moveDir != Vector2.zero)
+            lastMoveDir = moveDir.normalized;
+
+        float angle = Mathf.Atan2(lastMoveDir.y, lastMoveDir.x) * Mathf.Rad2Deg;
+
+        // ✅ CORREGIDO
+        flashlight.transform.rotation = Quaternion.Lerp(
+            flashlight.transform.rotation,
+            Quaternion.Euler(0, 0, angle - 90f),
+            Time.deltaTime * rotationSpeed
+        );
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isOn = !isOn;
+            flashlight.enabled = isOn;
+        }
+    }
+}
