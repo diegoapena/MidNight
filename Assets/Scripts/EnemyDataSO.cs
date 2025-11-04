1723e460b8656142de13f76c36df9b52b0f5a835
diegoapena <diego.apena@tecsup.edu.pe>
2025-11-04 18:16:44 -0500

    comit

--- Assets/Scripts/EnemyDataSO.cs
+++ Assets/Scripts/EnemyDataSO.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyDataSO", menuName = "MidNight/Scripts/EnemyDataSO")]
 public class EnemyDataSO : ScriptableObject
 {
+    public enum Fases
+    {
+        Passive,
+        Aggressive,
+        Defensive
+    }
     public string EnemyName;
     public ulong ID;
     public int Health;
