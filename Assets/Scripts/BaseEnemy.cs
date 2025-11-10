133ba82b1c52ebd8d93ceb06a89c250ac8e81038
diegoapena <diego.apena@tecsup.edu.pe>
2025-11-09 21:21:44 -0500

    cambio

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class BaseEnemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private string enemyName;
+    private int health;
+    private int damage;
+    private float speed;
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize(EnemyDataSO data)
     {
-        
+        enemyName = data.EnemyName;
+        health = data.Health;
+        damage = data.Damage;
+        speed = data.Speed;
+
+        Debug.Log($"Iniciado enemigo: {enemyName} con {health} de salud, {damage} de daño y {speed} de velocidad.");
     }
 }
