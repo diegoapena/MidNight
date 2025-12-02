97da64a3926e08f4e433472f8bb77efc87cc0ec4
rolandoparedes-sketch <rolando.paredes@tecsup.edu.pe>
2025-12-02 00:01:33 -0500

    MENU DE PAUSA

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+    public GameObject pauseUI; // Arrastra tu panel de pausa aquí
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
